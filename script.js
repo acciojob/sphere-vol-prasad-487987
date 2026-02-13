935d6e5f8094f3e87268ae551ccfc65542df1cd5
Prasad Yemekar <emekarprasad@gmail.com>
2026-02-13 16:00:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function volume_sphere() {
+	e.preventdefault();
     //Write your code here
+	let radius = document.getElementById("MyForm").value;
+
+	// By default whatever we take input from frontend ,
+	// It comes in string from . So whenever we want to take input 
+	// of number we have to convert string input in number .
+
+	radius = Number(radius);
+
+	if(isNaN(radius) || radius < 0){
+		document.getElementById("volume").value = NaN;
+	}
+
+	let volume = (4/3) * Math.pi *Math.pow(radius);
+	// It keeps value till 4 decimal number 
+	volume = volume.toFixed(4);
+
+	document.getElementById("volume").value = volume;
+	
   
 } 
 
