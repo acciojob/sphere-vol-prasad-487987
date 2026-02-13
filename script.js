25a8473c4aac09dbb8ca349643fe02ccec050d56
Prasad Yemekar <emekarprasad@gmail.com>
2026-02-13 16:17:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,51 +1,84 @@
-// function volume_sphere(e) {
-// 	e.preventdefault();
-//     //Write your code here
-// 	let radius = document.getElementById("radius").value;
+function volume_sphere(e) {
+	e.preventDefault();
+    //Write your code here
+	let radius = document.getElementById("radius").value;
 
-// 	// By default whatever we take input from frontend ,
-// 	// It comes in string from . So whenever we want to take input 
-// 	// of number we have to convert string input in number .
+	// By default whatever we take input from frontend ,
+	// It comes in string from . So whenever we want to take input 
+	// of number we have to convert string input in number .
 
-// 	radius = Number(radius);
+	radius = Number(radius);
 
-// 	if(isNaN(radius) || radius < 0){
-// 		document.getElementById("volume").value = NaN;
-// 		return false;
-// 	}
+	if(isNaN(radius) || radius < 0){
+		document.getElementById("volume").value = NaN;
+		return false;
+	}
 
-// 	let volume = (4/3) * Math.PI *Math.pow(radius,3);
-// 	// It keeps value till 4 decimal number 
-// 	volume = volume.toFixed(4);
+	let volume = (4/3) * Math.PI *Math.pow(radius,3);
+	// It keeps value till 4 decimal number 
+	volume = volume.toFixed(4);
 
-// 	document.getElementById("volume").value = volume;
+	document.getElementById("volume").value = volume;
 	
   
-// } 
+} 
 
+window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+
+
+// function volume_sphere(e) {
+// //Write your code here
+// e.preventDefault();
+// let radius = document.getElementById("radius").value;
+
+
+// radius = Number(radius);
+
+// if(isNaN(radius) || radius < 0) {
+// 	document.getElementById("volume").value = NaN;
+// 	return false;
+// }
+
+// let volume = (4/3) * Math.PI * Math.pow(radius, 3);
+
+// volume = volume.toFixed(4);
+
+// document.getElementById("volume").value = volume;
+
+
+// }
 // window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
 
 
-function volume_sphere(e) {
-//Write your code here
-e.preventDefault();
-let radius = document.getElementById("radius").value;
 
 
-radius = Number(radius);
-
-if(isNaN(radius) || radius < 0) {
-	document.getElementById("volume").value = NaN;
-	return false;
-}
-
-let volume = (4/3) * Math.PI * Math.pow(radius, 3);
-
-volume = volume.toFixed(4);
-
-document.getElementById("volume").value = volume;
 
 
-}
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
